3b9c1da5f0e01a1ba53ae85fd561b1f7d22b95dc
Jesus Velasco <jesus.velasco@babel.es>
2016-03-22 13:11:00 +0100

    Carga de peliculas con una promesa

--- scripts/controllers/MoviesListController.js
+++ scripts/controllers/MoviesListController.js
@@ -1,42 +1,30 @@
-angular.module("moviedb").controller("MoviesListController", ["$scope", function($scope) {
+angular.module("moviedb").controller("MoviesListController", 
+    ["$scope", "$log", "MovieService", 
+    function ($scope, $log, MovieService) {
 
-    // Scope init
-    $scope.uiState = 'ideal';
+        // Scope init
+        $scope.uiState = 'blank';
+        $scope.model = [];
 
-    $scope.model = [{
-        "title": "Deadpool",
-        "poster_url": "https://image.tmdb.org/t/p/w185/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
-        "rating": 7.2,
-        "release_date": "2016-02-19"
-    }, {
-        "title": "The Hunger Games: Mockingjay - Part 2",
-        "poster_url": "https://image.tmdb.org/t/p/w185/nN4cEJMHJHbJBsp3vvvhtNWLGqg.jpg",
-        "rating": 6.8,
-        "release_date": "2015-11-27"
-    }, {
-        "title": "Batman v Superman: Dawn of Justice",
-        "poster_url": "https://image.tmdb.org/t/p/w185/6bCplVkhowCjTHXWv49UjRPn0eK.jpg",
-        "rating": 5.1,
-        "release_date": "2016-03-23"
-    }, {
-        "title": "Mad Max: Fury Road",
-        "poster_url": "https://image.tmdb.org/t/p/w185/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-        "rating": 7.4,
-        "release_date": "2015-05-15"
-    }, {
-        "title": "Zootopia",
-        "poster_url": "https://image.tmdb.org/t/p/w185/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
-        "rating": 7.4,
-        "release_date": "2016-02-12"
-    }];
+        // Controller start
+        $scope.uiState = 'loading';
+        MovieService.getMovies().then(
+            // promesa resuelta
+            function(data){
+                $log.log("SUCCESS", data);
+                $scope.model = data;
+                if ($scope.model.length == 0) {
+                    $scope.uiState = 'blank';
+                } else {
+                    $scope.uiState = 'ideal';
+                }
+            },
 
-    // Scope watchers
-    $scope.$watch("model", function(newValue, oldValue){
-        if (newValue.length == 0){
-            $scope.uiState = 'blank';
-        }
-        else {
-            $scope.uiState = 'ideal';
-        }
-    })
-}]);
+            // promesa rechazada
+            function(data){
+                $log.error("ERROR", data);
+                $scope.uiState = 'error';
+            }
+        );
+    }]
+);
