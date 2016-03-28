9135b15b31da33565f6f4f1c0bfeefdea8f286b6
Jesus Velasco <jesus.velasco@babel.es>
2016-03-28 11:23:29 +0200

    Añadido angular-sanitize y meta para fijar tamaño

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // Defino el módulo "moviedb"
-angular.module("moviedb", ['ngRoute', "URL"]).config(
+angular.module("moviedb", ['ngRoute', "ngSanitize", "URL"]).config(
     ["$routeProvider", "paths", function( $routeProvider, paths){
 
         // Configuración de Route Provider
